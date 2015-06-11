177736b548e9e1a6d0c1c65e686c888052c9cb12
Nick Hibberd <nhibberd@gmail.com>
2015-06-11 17:37:24 +1000

    work

--- x-unix/test/Test/IO/X/System/Posix/Fcntl.hs
+++ x-unix/test/Test/IO/X/System/Posix/Fcntl.hs
@@ -6,25 +6,19 @@ module Test.IO.X.System.Posix.Fcntl where
 import           Data.Bool
 
 import           Control.Applicative
-import           Control.Concurrent
 import           Control.Monad
 
 import           Data.Function
 import           Data.Text
-import           Data.Monoid
 
 import           Disorder.Core.IO
 import           Disorder.Corpus
 
 import           System.IO
-import           System.FilePath
 import           System.IO.Temp
+import           System.FilePath
 
 import           Test.QuickCheck
-import           Text.Show
-import           System.Posix.IO
-
-import           X.System.Posix.Fcntl
 
 prop_foo :: Bool -> Property
 prop_foo b =
@@ -32,8 +26,9 @@ prop_foo b =
 
 prop_allocate :: Property
 prop_allocate = forAll (elements southpark) $ \s -> testIO $ do
---  withSystemTempDirectory "fcntl" $ \tmp -> do
-    let file = "/tmp/nick/" </> unpack s
+  withSystemTempDirectory "fcntl" $ \tmp -> do
+--    let file = "/tmp/nick/" </> unpack s
+    let file = tmp </> unpack s
     withFile file WriteMode $ \h -> do
       hSetFileSize h 100
     r <- withFile file ReadMode $ \h ->
