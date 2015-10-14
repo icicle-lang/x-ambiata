e1ee1ac9b3196cec2b0190fe68593215949d6842
Huw Campbell <huw.campbell@ambiata.com>
2015-10-14 13:51:01 +1100

    Don't show Missing error on empty

--- x-optparse/src/X/Options/Applicative.hs
+++ x-optparse/src/X/Options/Applicative.hs
@@ -27,6 +27,7 @@ import           Data.Int (Int)
 import           Data.Function
 import           Data.String (String)
 import           Data.Text as T
+import           Data.Monoid
 
 import           Options.Applicative as X
 import           Options.Applicative.Types as X
@@ -66,7 +67,12 @@ command' label description parser =
 --   default behaviour a bit better.
 dispatch :: Parser a -> IO a
 dispatch p = getArgs >>= \x -> case x of
-  [] -> customExecParser (prefs showHelpOnError)  (info (p <**> helper) idm)
+  [] -> let -- We don't need to see the Missing error if we're getting the whole usage string.
+            removeError' (h, e, c) = (h { helpError = mempty }, e, c)
+            removeError (Failure (ParserFailure failure)) = Failure (ParserFailure ( removeError' <$> failure ))
+            removeError a = a
+        in  execParserPure (prefs showHelpOnError) (info (p <**> helper) idm) <$> getArgs
+            >>= handleParseResult . removeError
   _  -> execParser (info (p <**> helper) idm)
 
 -- | orDieWithCode with an exit code of 1 in case of an error
