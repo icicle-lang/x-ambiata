8c18d11f4020bdd6aa6f09703ed9da14f94f359f
Jacob Stanley <jacob@stanley.io>
2016-03-09 11:14:13 +1100

    Add newtype Disaster wrapper for SomeException

--- x-exception/src/X/Control/Monad/Catch.hs
+++ x-exception/src/X/Control/Monad/Catch.hs
@@ -2,20 +2,51 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
 module X.Control.Monad.Catch (
-    bracketF
+    Disaster(..)
+  , bracketF
   , hushM
   ) where
 
 import           Control.Monad (return, liftM)
 import           Control.Monad.Catch (catchAll, throwM, handle)
-import           Control.Monad.Catch (Exception(..), SomeException)
+import           Control.Monad.Catch (Exception(..), SomeException(..))
 import           Control.Monad.Catch (MonadCatch(..), MonadMask(..))
 
-import           Data.Bool (Bool)
+import           Data.Bool (Bool(..))
 import           Data.Either (Either(..), either)
-import           Data.Function (($), const, id)
+import           Data.Eq (Eq(..))
+import           Data.Function (($), (.), const, id)
 import           Data.Maybe (Maybe(..))
+import           Data.Ord (Ord(..))
+import           Data.Typeable (typeOf)
 
+import           GHC.Show (appPrec)
+
+import           Text.Show (Show(..), showParen, showString, showChar)
+
+
+-- | Newtype wrapper for 'SomeException' which provides an 'Eq' instance which
+--   says no two disasters are equal. It also provides a 'Show' instance
+--   containing only valid Haskell 98 tokens.
+--
+--   This is useful for embedding 'SomeException' in a sensible error type that
+--   has equality for all the other cases.
+newtype Disaster =
+  Disaster {
+      unDisaster :: SomeException
+    }
+
+instance Eq Disaster where
+  (==) _ _ =
+    False
+
+instance Show Disaster where
+  showsPrec prec (Disaster (SomeException e)) =
+    showParen (prec > appPrec) $
+      showString "Disaster " .
+      showsPrec 11 (typeOf e) .
+      showChar ' ' .
+      showsPrec 11 (show e)
 
 data BracketResult a =
     BracketOk a
