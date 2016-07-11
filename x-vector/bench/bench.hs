d51b1fa7dd2ed7225fc2026a52766c413d08f687
Amos Robinson <amos.robinson@gmail.com>
2016-07-11 14:56:09 +1000

    add vector merge to benchmark

--- x-vector/bench/bench.hs
+++ x-vector/bench/bench.hs
@@ -16,16 +16,28 @@ import           System.IO (IO)
 import qualified X.Data.Vector as Boxed
 import qualified X.Data.Vector.Generic as Generic
 import qualified X.Data.Vector.Unboxed as Unboxed
+import qualified X.Data.Vector.Stream  as Stream
 
 
 main :: IO ()
 main =
-    defaultMainWith config .
-      fmap (uncurry bgroup) .
-      Map.toList .
-      Map.fromListWith (flip mappend) .
-      fmap (second (:[])) $
-      concatMap benchmarks [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+    defaultMainWith config
+     [ bgroup "Transpose" allTranspose
+     , bgroup "Merge" allMerge
+     ]
+ where
+  allTranspose = invertMap $
+   concatMap transposeBenchmarks [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+
+  allMerge = invertMap $
+   concatMap mergeBenchmarks $ fmap (*10) [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000]
+
+  invertMap =
+   fmap (uncurry bgroup) .
+   Map.toList .
+   Map.fromListWith (flip mappend) .
+   fmap (second (:[]))
+
 
 config :: Config
 config =
@@ -34,8 +46,8 @@ config =
     , csvFile = Just "dist/build/x-vector-bench.csv"
     }
 
-benchmarks :: Int -> [(String, Benchmark)]
-benchmarks size =
+transposeBenchmarks :: Int -> [(String, Benchmark)]
+transposeBenchmarks size =
   withMatrix size $ \list vec ->
     [ ("Data.List.transpose/list", bench (renderSize size) $ nf List.transpose list)
     , ("X.Data.Vector.Generic.transpose/vector", bench (renderSize size) $ nf Generic.transpose vec)
@@ -45,6 +57,18 @@ benchmarks size =
       [("Data.List.transpose/vector", bench (renderSize size) $ nf vecListTranspose vec)]
     else
       []
+
+mergeBenchmarks :: Int -> [(String, Benchmark)]
+mergeBenchmarks size =
+  withPairs size $ \list1 list2 vec1 vec2 ->
+    [ ("list", bench (renderSize size) $ nf3 Stream.mergeList fun list1 list2)
+    , ("vector", bench (renderSize size) $ nf3 Generic.merge fun vec1 vec2)
+    , ("vector-convert-list", bench (renderSize size) $ nf3 (\f a b -> Unboxed.fromList $ Stream.mergeList f (Unboxed.toList a) (Unboxed.toList b)) fun vec1 vec2)
+    ]
+ where
+  fun = Stream.mergePullOrd id
+  nf3 f x y z = nf (f x y) z
+
 
 renderSize :: Int -> String
 renderSize n =
@@ -68,3 +92,15 @@ withMatrix size f =
     vec = fmap Unboxed.fromList $ Boxed.fromList list
   in
     list `deepseq` vec `deepseq` f list vec
+
+withPairs :: Int -> ([Int] -> [Int] -> Unboxed.Vector Int -> Unboxed.Vector Int -> a) -> a
+withPairs size f =
+  let
+    list1 :: [Int]
+    list1 = [0..size]
+    list2 = List.take size $ List.drop (size `div` 2) $ List.cycle list1
+
+    vec1 = Unboxed.fromList $ list1
+    vec2 = Unboxed.fromList $ list2
+  in
+    list1 `deepseq` list2 `deepseq` vec1 `deepseq` vec2 `deepseq` f list1 list2 vec1 vec2
