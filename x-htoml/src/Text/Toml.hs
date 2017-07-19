e4e7ad0f7df552426d344716e17b1271cd47dc12
Jacob Stanley <jacob@stanley.io>
2017-07-19 13:05:13 +1000

    Import htoml-0.1.0.3 in to x-htoml

---
+++ x-htoml/src/Text/Toml.hs
@@ -0,0 +1,15 @@
+module Text.Toml where
+
+import           Prelude          hiding (readFile)
+
+import           Data.Text        (Text)
+import           Text.Parsec
+
+import           Text.Toml.Parser
+
+
+-- | Parse a 'Text' that results in 'Either' a 'String'
+-- containing the error message, or an internal representation
+-- of the document in the 'Toml' data type.
+parseTomlDoc :: String -> Text -> Either ParseError Table
+parseTomlDoc inputName input = parse tomlDoc inputName input
